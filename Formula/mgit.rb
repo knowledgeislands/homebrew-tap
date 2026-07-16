d6e0467d8829a00f402c63278f8935ac83046dbc
Kris Brown <kris@kris.me.uk>
2026-07-16 01:20:03 +0100

    feat(mgit): update formula to v0.4.0

--- Formula/mgit.rb
+++ Formula/mgit.rb
@@ -1,8 +1,8 @@
 class Mgit < Formula
   desc "Run a git command across many repositories at once"
   homepage "https://github.com/knowledgeislands/tools-mgit"
-  url "https://github.com/knowledgeislands/tools-mgit/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "c874f6ce1f39626508ea451f3c5e422c812e9659716bbb210d8fc450042cec88"
+  url "https://github.com/knowledgeislands/tools-mgit/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "9758ee0210bbe3b820400f0a71b826f60c13cdc8fe76a0b183dd845a49f3106c"
   license "MIT"
 
   def install
