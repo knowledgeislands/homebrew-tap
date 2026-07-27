8e135389ffa937ec324e63a89afec931d52d825b
Kris Brown <kris@kris.me.uk>
2026-07-27 01:38:08 +0100

    feat(mgit): release v0.5.2

--- Formula/mgit.rb
+++ Formula/mgit.rb
@@ -1,12 +1,13 @@
 class Mgit < Formula
   desc "Run a git command across many repositories at once"
   homepage "https://github.com/knowledgeislands/tools-mgit"
-  url "https://github.com/knowledgeislands/tools-mgit/archive/refs/tags/v0.5.1.tar.gz"
-  sha256 "3765c0ad19e6cca81298d6cfa095f21915cf9f6d436e62a64e026cde289e441c"
+  url "https://github.com/knowledgeislands/tools-mgit/archive/refs/tags/v0.5.2.tar.gz"
+  sha256 "0148c3b7c38400acdb3cb16c4299eb63e8815a478529a3cd932e651cb378895a"
   license "MIT"
 
   def install
     bin.install "bin/mgit"
+    man1.install "man/mgit.1"
   end
 
   test do
