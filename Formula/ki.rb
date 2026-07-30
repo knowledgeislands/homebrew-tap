a354c9074d8a0ed9514b39690f999fb0ad7ac374
Kris Brown <kris@kris.me.uk>
2026-07-30 23:29:31 +0100

    chore: update ki to v0.2.13

--- Formula/ki.rb
+++ Formula/ki.rb
@@ -1,25 +1,24 @@
 class Ki < Formula
   desc "Knowledge Islands command-line interface"
   homepage "https://github.com/knowledgeislands/tools-ki"
-  version "0.2.11"
   license "MIT"
 
   on_arm do
     on_macos do
-      url "https://github.com/knowledgeislands/tools-ki/releases/download/v0.2.11/ki-v0.2.11-darwin-arm64.tar.gz"
-      sha256 "409dc48d5105a8c2ff95b7f6c7acd5989a62eb1a4c672b50f17ab077f2d8cc0b"
+      url "https://github.com/knowledgeislands/tools-ki/releases/download/v0.2.13/ki-v0.2.13-darwin-arm64.tar.gz"
+      sha256 "8ccea8f5c6c51f9902877e8a4e3b976b1c84e3fb4f06f241be16ad485cf5a539"
     end
   end
 
   on_intel do
     on_macos do
-      url "https://github.com/knowledgeislands/tools-ki/releases/download/v0.2.11/ki-v0.2.11-darwin-x64.tar.gz"
-      sha256 "7460748918015213d9116faee6c9c8d8c62f5aa915239c9bfdeece75ee1983d1"
+      url "https://github.com/knowledgeislands/tools-ki/releases/download/v0.2.13/ki-v0.2.13-darwin-x64.tar.gz"
+      sha256 "aaabc582ea55345bed40377286c83bf116742d433ca14d6d99037293ddb258db"
     end
 
     on_linux do
-      url "https://github.com/knowledgeislands/tools-ki/releases/download/v0.2.11/ki-v0.2.11-linux-x64.tar.gz"
-      sha256 "66807b0c92c5efa8c3c21d00ff3ced0490e7b5b5823821cb1d7ae304a3cabe08"
+      url "https://github.com/knowledgeislands/tools-ki/releases/download/v0.2.13/ki-v0.2.13-linux-x64.tar.gz"
+      sha256 "1ba94fd00c8e7b13968d1bc08ed4e4dbbfd5543fc812d578e95aa2baaedd3fa4"
     end
   end
 
