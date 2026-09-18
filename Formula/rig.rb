1964e4d9146a0c5b0493a0d2503fc7eedda57693
Kris Brown <kris@kris.me.uk>
2026-09-18 07:44:21 +0100

    chore(rig): update formula v0.2.0

--- Formula/rig.rb
+++ Formula/rig.rb
@@ -1,8 +1,8 @@
 class Rig < Formula
   desc "Describe and manage a person's working setup"
   homepage "https://github.com/knowledgeislands/tools-rig"
-  url "https://github.com/knowledgeislands/tools-rig/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "5edcbe3b0a753b499c7dcb1b62640957c61f24b378cd2d3a520af01e10fd2bf6"
+  url "https://github.com/knowledgeislands/tools-rig/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "16fcfaf586e477f909ab7241ed69b2354d1213fb1f49cdf17fa8075a955eb335"
   license "MIT"
 
   def install
